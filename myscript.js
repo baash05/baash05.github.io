2fe7fd16e5a5e05e1f317663c3fc2ec0730dd3b5
baash05 <david.rawk@gmail.com>
2018-03-03 20:53:05 +1100

    added index

---
+++ myscript.js
@@ -0,0 +1,11 @@
+function storeText(){
+  var element = document.getElementById("myTextBox");
+  localStorage.setItem("thoughts", element.value || "");
+  element.focus();
+}
+
+function loadText(){
+  var element = document.getElementById("myTextBox");
+  element.value = localStorage.getItem("thoughts");
+  element.focus();
+}
